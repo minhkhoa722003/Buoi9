5367346d3b47296b637c41b4972907b705f2796f
Nguyen Minh Khoa <mjnhkhoa722003@gmail.com>
2023-11-01 16:22:56 +0700

    Buoi9

---
+++ Buoi9/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Buoi9
+{
+    public partial class Form1 : Form
+    {
+        KetNoi kn = new KetNoi();
+        SqlConnection connsql;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connsql = kn.connect;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(connsql.State == ConnectionState.Closed)
+                {
+                    connsql.Open();
+                }
+                string insertString;
+                insertString = "insert into Khoa values('" + textBox1.Text + "',N'" + textBox2.Text + "')";
+                SqlCommand cmd = new SqlCommand(insertString, connsql);
+                cmd.ExecuteNonQuery();
+                if(connsql.State == ConnectionState.Open)
+                {
+                    connsql.Close();
+                }
+                MessageBox.Show("Thanh cong");
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("That bai");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connsql.State == ConnectionState.Closed)
+                {
+                    connsql.Open();
+                }
+                string updateString = "update Khoa set TenKhoa='" + textBox2.Text + "' where MaKhoa='" + textBox1.Text + "'";
+                SqlCommand cmd = new SqlCommand(updateString, connsql);
+                cmd.ExecuteNonQuery();
+                if (connsql.State == ConnectionState.Open)
+                {
+                    connsql.Close();
+                }
+                MessageBox.Show("Thanh cong");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("That bai");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connsql.State == ConnectionState.Closed)
+                {
+                    connsql.Open();
+                }
+                string deleteString = "delete Khoa where MaKhoa='" + textBox1.Text + "'";
+                SqlCommand cmd = new SqlCommand(deleteString, connsql);
+                cmd.ExecuteNonQuery();
+                if (connsql.State == ConnectionState.Open)
+                {
+                    connsql.Close();
+                }
+                MessageBox.Show("Thanh cong");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("That bai");
+            }
+        }
+
+
+    }
+}
---
+++ Buoi9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Buoi9
+{
+    public class KetNoi
+    {
+        public SqlConnection connect;
+        public KetNoi()
+        {
+            SqlConnection connect = new SqlConnection("Data Source = A209PC13; Initial Catalog = QLSinhVien; Integrated Security = True");
+
+        }
+        public KetNoi(string strcn)
+        {
+            connect = new SqlConnection(strcn);
+        }
+
+    }
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+    }
+}
